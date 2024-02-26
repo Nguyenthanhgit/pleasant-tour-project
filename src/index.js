0c3b61d40f3cce64b7cc47c152a5500b3652f6b0
Nguyenthanhgit <thanhxuan12086@gmail.com>
2024-02-26 20:58:26 +0700

    add Folder Pages

--- src/index.js
+++ src/index.js
@@ -3,11 +3,71 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
+import About from './Pages/About';
+import Home from './Pages/Home';
+import Services from './Pages/Services';
+import Packages from './Pages/Packages';
+import Contact from './Pages/Contact';
+import Destination from './Pages/Destination';
+import Booking from './Pages/Booking';
+import Team from './Pages/Team';
+import Testimonial from './Pages/Testimonial';
+import Pages404 from './Pages/Page404';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/about',
+        element: <About />,
+      },
+      {
+        path: '/services',
+        element: <Services />,
+      },
+      {
+        path: '/packages',
+        element: <Packages />,
+      },
+      {
+        path: '/destination',
+        element: <Destination />,
+      },
+      {
+        path: '/booking',
+        element: <Booking />,
+      },
+      {
+        path: '/team',
+        element: <Team />,
+      },
+      {
+        path: '/testimonial',
+        element: <Testimonial />,
+      },
+      {
+        path: '/404',
+        element: <Pages404 />,
+      },
+      {
+        path: '/contact',
+        element: <Contact />,
+      },
+    ],
+  },
+]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
